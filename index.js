bc697afa15b3516364c0b550b2b4dca35dda1840
ah-munna <ahmunna.developer@gmail.com>
2026-09-09 20:41:17 +0600

    fix: register SeeYouLater alias and update native main component name to ContextEngine

--- index.js
+++ index.js
@@ -1,13 +1,3 @@
-// /**
-//  * @format
-//  */
-
-// import {AppRegistry} from 'react-native';
-// import App from './App';
-// import {name as appName} from './app.json';
-
-// AppRegistry.registerComponent(appName, () => App);
-
 /**
  * @format
  */
@@ -18,4 +8,9 @@ import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
 
+// Primary registration for ContextEngine
 AppRegistry.registerComponent(appName, () => App);
+
+// Backward-compatible alias for existing native builds expecting "SeeYouLater"
+AppRegistry.registerComponent('SeeYouLater', () => App);
+
